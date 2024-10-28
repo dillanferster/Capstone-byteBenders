7cbaa46eb438fbb5774fdb91e899aea5ff7e45bb
Dillan <dillyferster@gmail.com>
2024-10-28 11:34:51 -0600

    added testing dependacies
    intial setup, added testing dependacies, added test file

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,11 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./tests/main.test.js",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "/src"),
