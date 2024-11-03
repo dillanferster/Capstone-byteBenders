d6bf49c8f99dee2b57f1f2e81c5a4de84ae39ce2
gigi-vu2804 <gigivu2804@gmail.com>
2024-11-03 11:43:09 -0700

    added gantt chart for project

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,16 +5,12 @@ import "./tailwind.css";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { msalInstance } from "./outlook/authConfig"; // Import the initialized MSAL instance
-import { MsalProvider } from "@azure/msal-react"; // Optional, if you install msal-react for simplified context
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <MsalProvider instance={msalInstance}>
-        <App />
-      </MsalProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
