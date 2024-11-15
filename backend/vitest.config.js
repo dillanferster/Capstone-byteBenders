23859902711529d22f227b61a42e941cc429ffb7
Hermela-yimer1 <141375883+Hermela-yimer1@users.noreply.github.com>
2024-11-15 14:16:01 -0700

    Revert "add test reporter & coverage on backend"
    This reverts commit c9e62d6224bd366798c470e7c5e5df09db577c2c.

--- backend/vitest.config.js
+++ backend/vitest.config.js
@@ -4,17 +4,5 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "node",
-    reporters: [
-      "default",
-      [
-        "json",
-        {
-          outputFile: "./tests/results/all-tests-results.json",
-        },
-      ],
-      "./tests/utils/multiTestReporter.js",
-    ],
-    include: ["tests/**/*.{test,spec}.{js,jsx}"],
-    exclude: ["node_modules", "dist"],
   },
 });
